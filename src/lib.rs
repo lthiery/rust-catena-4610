b3b34da8e609794b4482564e76679deb6eeade88
Hanno Braun <hanno@braun-embedded.com>
2019-08-15 12:27:48 +0200

    Add minimal board support crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Rust Board Support Crate (BSC) for the MCCI Catena 4610
+//!
+//! More infos about the MCCI Catena 4610:
+//! https://store.mcci.com/collections/lorawan-iot-and-the-things-network/products/mcci-catena-4610-integrated-node-for-lorawan-technology
+//!
+//! This Board Support Crate is in its very early stages. Documentation is
+//! sparse and features are basic. Pull requests welcome!
+
+
+#![no_std]
+
+
+pub use stm32l0xx_hal as hal;
